f7b3412159ae6fbd2c0d31f4805862f7786f59dc
Vladi2811 <vladgoia2811@gmail.com>
2019-04-11 00:58:06 +0200

    Line detection done

--- Android/app/src/main/java/com/veprojects/musicscanner/MainActivity.java
+++ Android/app/src/main/java/com/veprojects/musicscanner/MainActivity.java
@@ -1,13 +1,27 @@
 package com.veprojects.musicscanner;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.Image;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private ArrayList<Bitmap> imgs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        imgs = new ArrayList<Bitmap>();
+        imgs.add(BitmapFactory.decodeResource(this.getResources(),R.drawable.penta10));
         setContentView(R.layout.activity_main);
+        MusicSheet ms = new MusicSheet(imgs);
+        ImageView imgExpose = findViewById(R.id.imageView);
+        imgExpose.setImageBitmap(ms.readLines(imgs.get(0)));
+
+
     }
 }
