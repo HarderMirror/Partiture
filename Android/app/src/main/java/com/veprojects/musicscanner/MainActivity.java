7639123c9d719df51c0d8d799ef7ebca52aad650
Vladi2811 <vladgoia2811@gmail.com>
2019-04-16 22:33:07 +0200

    Python IA

--- Android/app/src/main/java/com/veprojects/musicscanner/MainActivity.java
+++ Android/app/src/main/java/com/veprojects/musicscanner/MainActivity.java
@@ -1,5 +1,6 @@
 package com.veprojects.musicscanner;
 
+import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.media.Image;
@@ -16,7 +17,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         imgs = new ArrayList<Bitmap>();
-        imgs.add(BitmapFactory.decodeResource(this.getResources(),R.drawable.penta10));
+        imgs.add(BitmapFactory.decodeStream(this.getResources().openRawResource(R.raw.penta8)));
+        //imgs.add(BitmapFactory.decodeResource(this.getResources(),R.raw.penta10));
         setContentView(R.layout.activity_main);
         MusicSheet ms = new MusicSheet(imgs);
         ImageView imgExpose = findViewById(R.id.imageView);
--- Android/app/src/main/java/com/veprojects/musicscanner/MusicSheet.java
+++ Android/app/src/main/java/com/veprojects/musicscanner/MusicSheet.java
@@ -2,6 +2,7 @@ package com.veprojects.musicscanner;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.Bitmap;
+import android.util.TimingLogger;
 import android.widget.ImageView;
 
 import java.util.ArrayList;
@@ -34,24 +35,30 @@ public class MusicSheet {
         this.readLines(this.getImgs().get(0));
     }
     public Bitmap readLines(Bitmap img){
+
         Bitmap.Config conf = Bitmap.Config.ARGB_8888;
         Bitmap newImg = Bitmap.createBitmap(img.getWidth(),img.getHeight(),conf);
         int[] pixels = new int[img.getWidth()];
+        int lineas = 0;
         for(int y = 0; y < img.getHeight() ; y++){
             img.getPixels(pixels,0,img.getWidth(),0,y,img.getWidth(),1);
             float prob = probabilityOfLine(pixels);
             //System.out.printf("Fila %d: %f\n",y,prob);
 
-            if(prob > 1000) {
+            if(prob > 20) {
+
                 for(int x = 0; x < img.getWidth(); x++){
                     newImg.setPixel(x,y,Color.argb(Color.alpha(pixels[x]),255,0,0));
 
                 }
+                lineas++;
+
             } else {
                 for(int x = 0; x < img.getWidth(); x++){
                     newImg.setPixel(x,y,Color.argb(Color.alpha(pixels[x]),0,0,255));
                 }
             }
+
 
             /*if(this.isBlack(pixel)){
 
@@ -62,33 +69,29 @@ public class MusicSheet {
 
 
         }
+
+        System.out.println("Lineas totales: " +lineas);
         return newImg;
     }
     private float probabilityOfLine(int[] pixels){
         float prob = 0;
-        int cont = pixels.length-1;
-        while(cont > 0 &&!this.isBlack(pixels[cont])) {
-            cont--;
-        }
-        int finalPixel = cont;
-        int init = -1;
-        for(int x = 0; x <= finalPixel; x++){
-            if(this.isBlack(pixels[x])){
-                init = x;
+
+
+        for(int x = 0; x < pixels.length-1; x+=2){
+
+            if(this.isBlack(pixels[x]) && this.isBlack(pixels[x+1])) {
+                prob+=2;
+            } else {
+                prob--;
             }
-            if(init!=-1){
-                if(this.isBlack(pixels[x])) {
-                    prob++;
-                } else {
-                    prob--;
-                }
-            }
+
         }
-        return  prob;
+        //System.out.printf("Final:%d   Init:%d\n",finalPixel,init);
+        return  prob < 0 ? 0 : prob/pixels.length*100;
     }
     private boolean isBlack(int pixel){
         int media = (Color.red(pixel) + Color.green(pixel) + Color.blue(pixel)) /3 + (255-Color.alpha(pixel));
 
-        return media < 254;
+        return media <= 140;
     }
 }
