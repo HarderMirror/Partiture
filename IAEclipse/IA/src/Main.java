9be98ad2535013a2d7c4d5e8ac01d455567adc34
Vladi2811 <vladgoia2811@gmail.com>
2019-04-23 18:43:12 +0200

    IA Progress

--- IAEclipse/IA/src/Main.java
+++ IAEclipse/IA/src/Main.java
@@ -3,6 +3,16 @@ import musicSheetCreator.MusicSheetCreator;
 
 public class Main {
 	public static void main(String[] args) {
-		new MusicSheetCreator();
+		//new MusicSheetCreator();
+		double init1 = System.currentTimeMillis();
+		double[][] pepe = np.random(224000, 1);
+		double[][] pepeW = np.random(224000, 1);
+		double init2 = System.currentTimeMillis();
+		
+		for(int i=0; i < 3; i++) {
+			np.multiply(pepe, pepeW);
+		}
+		double finalT = System.currentTimeMillis();
+		System.out.println("Time is: " + (finalT - init2));
 	}
 }
--- IAEclipse/IA/src/NotesAI.java
+++ IAEclipse/IA/src/NotesAI.java
@@ -1,10 +1,12 @@
 
 public class NotesAI {
+	private final int GROUPS_LENGHT = 10;
 	private double[] input;
 	private double[][] bias;
 	private double[][][] weights;
-	private double[][][] hiddenLayers;
-	
+	private double[][] hiddenLayers;
+	private double[] res;
+	private final double ALPHA_VALUE = -0.1; 
 
 	//[3 2 2] = 3 first layer, 2 second, 2 third
 	private int[] layers;
@@ -12,7 +14,15 @@ public class NotesAI {
 	public NotesAI(double[] input, int[] layers) {
 		this.setLayers(layers);
 		this.setInput(input);
+		this.createHiddenLayers();
 		this.setRandomWeights();
+		
+	}
+	public NotesAI(double[] input, int[] layers, double[][][] weights) {
+		this.setLayers(layers);
+		this.setInput(input);
+		this.createHiddenLayers();
+		this.setWeights(weights);
 		
 	}
 	
@@ -25,6 +35,15 @@ public class NotesAI {
 		}
 		return MultRes + bias[biasLayer][posFinalNeuron];
 	}
+	private double derivateOfZRespectBias() {
+		return 1;
+	}
+	private double derivateOfZRespectWeight(int weightLayer, int posY, int posX) {
+		return this.getWeights()[weightLayer][posY][posX];
+	}
+	private double derivateOfZRespectPrevNeuron() {
+		return this.get
+	}
 	private double sigmoid(double Z) {
 		return (double) (1 / (1 + (Math.pow(Math.E,-Z))));
 	}
@@ -34,23 +53,103 @@ public class NotesAI {
 	
 	
 	private void setRandomWeights() {
-		for(int i = 1; i < this.getLayers().length; i++ ) {
+		for(int i = 1; i < this.getLayers().length; i++) {
 			this.getWeights()[i-1] = np.random(this.getLayers()[i], this.getLayers()[i-1]);
 		}
 	}
 	private void createHiddenLayers() {
 		for(int i = 0; i < this.getLayers().length-2;i++) {
-			this.getHiddenLayers()[i] = np.random(1,this.getLayers()[i+1]);
+			this.getHiddenLayers()[i] = np.random(this.getLayers()[i+1],1)[0];
 		}
 		
 	}
 	
-	private double[] feedForward(int layerNeurons, int layerNum, double[] leftLayer) {
+	private double[] feedForward(int layerNum, double[] leftLayer) {
 		double[] res = new double[this.getLayers()[layerNum]];
-		for(int i = 0; i < layerNeurons; i++) {
+		for(int i = 0; i < this.getLayers()[layerNum]; i++) {
 			res[i] = this.sigmoid(this.Z(i, layerNum-1, layerNum-1, leftLayer));
 		}
 		return res;
+	}
+	private double cost(double[] testRes, double[] realRes) {
+		double cost = 0;
+		for(int i = 0; i < testRes.length; i++) {
+			cost = cost + Math.pow((testRes[i] - realRes[i]),2);
+		}
+		return cost;
+	}
+	private double derivateOfCost(double[] testRes, double[] realRes) {
+		double cost = 0;
+		for(int i = 0; i < testRes.length; i++) {
+			cost = cost + 2*(testRes[i] - realRes[i]);
+		}
+		return cost;
+	}
+	
+	private double derivateOfCostRespectWeight(int weightLayer, int posY, int posX,
+			double[] leftLayer, int posFinalNeuron, double[]testRes, double[]realRes) {
+		return this.derivateOfZRespectWeight(weightLayer, posY, posX) *
+				this.derivativeOfSigmoid(this.Z(posFinalNeuron, weightLayer, weightLayer, leftLayer)) *
+				this.derivateOfCost(testRes, realRes);    
+	}
+	private double derivateOfCostRespectBias(int weightLayer, int posY, int posX,
+			double[] leftLayer, int posFinalNeuron, double[]testRes, double[]realRes) {
+		return this.derivateOfZRespectBias() *
+				this.derivativeOfSigmoid(this.Z(posFinalNeuron, weightLayer, weightLayer, leftLayer)) *
+				this.derivateOfCost(testRes, realRes);    
+	}
+	private double derivateOfCostRespectPrevNeuron(int weightLayer, int posY, int posX, double[]testRes, double[]realRes) {
+		return this.derivateOfZRespectPrevNeuron (weightLayer, posY, posX) *
+				this.derivativeOfSigmoid(this.Z(posFinalNeuron, weightsNum, biasLayer, leftLayer)) *
+				this.derivateOfCost(testRes, realRes);    
+	}
+	
+	
+	private void backTrack() {
+		
+	}
+	
+	private void train(double[] input, double[] realRes) {
+		this.setInput(input);
+		for(int i = 0; i < this.getLayers().length; i++) {
+			if(i == 0) {
+				this.getHiddenLayers()[0] = this.feedForward(i+1, input);
+			}else if(i == this.getLayers().length-1){
+				this.setRes(this.feedForward(i+1,this.getHiddenLayers()[i-1]));
+			}
+			else{
+				this.getHiddenLayers()[i] = this.feedForward(i+1, this.getHiddenLayers()[i-1]);
+			}
+						
+		}
+		
+		//Until here we get the res by feedForward, now we have to get the cost
+		
+		double cost = this.cost(this.getRes(), realRes);
+		
+		//Now we have to backTrack to change weights, bias, and neurons
+		
+		//Change weights by layers
+		for(int i = this.getWeights().length-1; i >= 0; i++) {
+			
+			for(int j = 0; j < this.getWeights()[i].length; j++) {
+				for(int k = 0; k < this.getWeights()[i][j].length; k++) {
+					if(i != 0) {
+						this.getWeights()[i][j][k] = this.getWeights()[i][j][k] + this.ALPHA_VALUE * 
+								this.derivateOfCostRespectWeight(i, j, k, this.getHiddenLayers()[i-1],
+										j, this.getRes(), realRes);
+					}else {
+						this.getWeights()[i][j][k] = this.getWeights()[i][j][k] + this.ALPHA_VALUE * 
+								this.derivateOfCostRespectWeight(i, j, k, this.getInput(),
+										j, this.getRes(), realRes);
+						
+					}
+					
+				}
+			}
+		}
+		
+		
 	}
 	
 
@@ -91,13 +190,20 @@ public class NotesAI {
 		this.weights = weights;
 	}
 
-	public double[][][] getHiddenLayers() {
+	public double[][] getHiddenLayers() {
 		return hiddenLayers;
 	}
 
-	public void setHiddenLayers(double[][][] hiddenLayers) {
+	public void setHiddenLayers(double[][] hiddenLayers) {
 		this.hiddenLayers = hiddenLayers;
 	}
+	public double[] getRes() {
+		return res;
+	}
+	public void setRes(double[] res) {
+		this.res = res;
+	}
+	
 	
 
 	
