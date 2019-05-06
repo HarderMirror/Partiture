c109036f3fcf352848d8cfdd7812c221d0cce550
davidasecas1 <davidespejoantinolo@gmail.com>
2019-05-06 15:32:50 +0200

    Change model

--- IAEclipse/IA/net2.py
+++ IAEclipse/IA/net2.py
@@ -7,12 +7,12 @@ import numpy as np
 import cv2
 import os
 
-imagenes = 10000
+imagenes = 100
 RESCALE_VALUE = 5
 width = 1280//RESCALE_VALUE
 height = 175//RESCALE_VALUE
 
-numImgTest = 65
+numImgTest = 5
 
 x_train = []
 y_train = cv2.imread("y.jpg",0)
@@ -37,108 +37,107 @@ x_train = np.array(x_train).reshape(imagenes, height, width, 1)
 
 
 x_train = np.dot(x_train, 1/255)
+
 y_train = np.array(y_train)
 y_train = np.dot(y_train, 1/255)
+y_train = np.around(y_train)
+y_train = y_train.astype(int)
 
 
-model.add(tf.keras.layers.Conv2D(1, kernel_size=kernel_size, name='conv1_1', input_shape = input_shape))
-model.add(tf.keras.layers.Activation("relu"))
-model.add(tf.keras.layers.Conv2D(32, kernel_size=kernel_size, name='conv1_2'))
-model.add(tf.keras.layers.Activation("relu"))
+model.add(tf.keras.layers.Conv2D(32, kernel_size=kernel_size, name='conv1_1', input_shape = input_shape, activation=tf.nn.leaky_relu))
+model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+model.add(tf.keras.layers.Dropout(0.25))
+model.add(tf.keras.layers.Conv2D(64, kernel_size=kernel_size, name='conv1_2', activation=tf.nn.leaky_relu))
 model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
 model.add(tf.keras.layers.Dropout(0.25))
 print("Hola")
-# #2
-# model.add(tf.keras.layers.Conv2D(64, kernel_size=kernel_size,name='conv2_1'))
-# model.add(tf.keras.layers.Activation("relu"))
-# model.add(tf.keras.layers.Conv2D(64, kernel_size=kernel_size,name='conv2_2'))
-# model.add(tf.keras.layers.Activation("relu"))
+#2
+model.add(tf.keras.layers.Conv2D(128, kernel_size=kernel_size,name='conv2_1', activation=tf.nn.leaky_relu))
+# model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+# model.add(tf.keras.layers.Dropout(0.25))
+# model.add(tf.keras.layers.Conv2D(256, kernel_size=kernel_size,name='conv2_2', activation=tf.nn.leaky_relu))
 # model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
 # model.add(tf.keras.layers.Dropout(0.25))
 # # #3
-# model.add(tf.keras.layers.Conv2D(128, kernel_size=kernel_size,name='conv3_1'))
-# model.add(tf.keras.layers.Activation("relu"))
-# model.add(tf.keras.layers.Conv2D(128, kernel_size=kernel_size,name='conv3_2'))
-# model.add(tf.keras.layers.Activation("relu"))
+# model.add(tf.keras.layers.Conv2D(128, kernel_size=kernel_size,name='conv3_1', activation=tf.nn.leaky_relu))
+# model.add(tf.keras.layers.Conv2D(128, kernel_size=kernel_size,name='conv3_2', activation=tf.nn.leaky_relu))
 # model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
 # model.add(tf.keras.layers.Dropout(0.25))
 
 model.add( tf.keras.layers.Flatten())
+model.add(tf.keras.layers.Dense(1000, activation=tf.nn.leaky_relu))
 model.add(tf.keras.layers.Dropout(0.5))
 
-model.add(tf.keras.layers.Dense(numFinalNeurons))
-model.add(tf.keras.layers.Activation('softmax'))
+model.add(tf.keras.layers.Dense(numFinalNeurons, activation=tf.nn.sigmoid))
 
 
-# optimizer = tf.train.AdamOptimizer(learning_rate=0.0005)
+optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+
 model.compile(optimizer='adam',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 
 # #tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
-x_test = cv2.imread("images/661.jpg",0)
-x_test = np.array(x_test).reshape(1, height, width, 1)
-x_test = np.dot(x_test, 1/255)
 
-model.fit(x_train, y_train, epochs=5, shuffle=True, use_multiprocessing=True, verbose=True)
+
+model.fit(x_train, y_train, epochs=200, shuffle=True, use_multiprocessing=True, verbose=True)
 
 model.evaluate(x_train, y_train)
-pred = model.predict(x_test,verbose=1)
-print(pred)
 
 
 # SAVE MODEL----------------------------
 # serialize model to JSON
 model_json = model.to_json()
-with open("model.json", "w") as json_file:
+with open("model2.json", "w") as json_file:
     json_file.write(model_json)
 # serialize weights to HDF5
-model.save_weights("model.h5")
+model.save_weights("model2.h5")
 print("Saved model to disk")
 # ------------------------------------------
 
-# # LOAD MODEL----------------------------
-# json_file = open('model.json', 'r')
+# # # LOAD MODEL----------------------------
+# json_file = open('model2.json', 'r')
 # loaded_model_json = json_file.read()
 # json_file.close()
 # model = tf.keras.models.model_from_json(loaded_model_json)
 # # load weights into new model
-# model.load_weights("model.h5")
+# model.load_weights("model2.h5")
 # print("Loaded model from disk")
  
 # # # evaluate loaded model on test data
 # model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# x_test = cv2.imread("images/{}.jpg".format(numImgTest),0)
-# x_test = np.array(x_test).reshape(1, height, width, 1)
-# x_test = np.dot(x_test, 1/255)
-# pred = model.predict(x_test,verbose=1)[0]
-# step = 23
-# data = []
-# dataResult = []
-# numTypeNotes = 3
-# for i in range(0, len(pred), step):
-#   noteData = pred[i:i+step]
-#   max1 = np.argmax(noteData)
-#   noteData[max1] = -1
-#   max2 = np.argmax(noteData)
-#   if(max1 > max2):
-#     data.append([max2, max1-numTypeNotes])
-#   else:
-#     data.append([max1, max2-numTypeNotes])
+# ----------------------
+
+
+
+#test------------------
+x_test = cv2.imread("images/{}.jpg".format(numImgTest),0)
+x_test = np.array(x_test).reshape(1, height, width, 1)
+x_test = np.dot(x_test, 1/255)
+pred = model.predict(x_test,verbose=1)[0]
+numTypeNotes = 3
+positions = 20
+step = numTypeNotes + positions
+data = []
+dataResult = []
+# print(np.around(pred))
+for i in range(0, len(pred), step):
+  noteTypeData = pred[i:i+numTypeNotes]
+  maxTypeData = np.argmax(noteTypeData)
+  notePosData = pred[i+numTypeNotes:i+positions]
+  maxPosData = np.argmax(notePosData)
+  data.append([maxTypeData, maxPosData])
   
-#   noteDataResult = y_train[numImgTest][i:i+step]
-#   max1 = np.argmax(noteDataResult)
-#   noteDataResult[max1] = -1
-#   max2 = np.argmax(noteDataResult)
-#   if(max1 > max2):
-#     dataResult.append([max2, max1-numTypeNotes])
-#   else:
-#     dataResult.append([max1, max2-numTypeNotes])
+  noteTypeDataRes = y_train[numImgTest][i:i+numTypeNotes]
+  maxTypeDataRes = np.argmax(noteTypeDataRes)
+  notePosDataRes = y_train[numImgTest][i+numTypeNotes:i+positions]
+  maxPosDataRes = np.argmax(notePosDataRes)
+  dataResult.append([maxTypeDataRes, maxPosDataRes])
 
 
-# print(data)
-# print(dataResult)
-# #------------------------------------
+print(data)
+print(dataResult)
+# # #------------------------------------
 
 
 
--- IAEclipse/IA/netNumImages.py
+++ IAEclipse/IA/netNumImages.py
@@ -7,6 +7,7 @@ import numpy as np
 import cv2
 import os
 
+numImage = 21
 imagenes = 1000
 RESCALE_VALUE = 5
 width = 1280//RESCALE_VALUE
@@ -36,82 +37,99 @@ x_train = np.array(x_train).reshape(imagenes, height, width, 1)
 
 
 x_train = np.dot(x_train, 1/255)
+x_train = np.around(x_train)
+x_train = x_train.astype(int)
+
 y_train = np.array(y_train)
 y_train = np.dot(y_train, 1/255)
+y_train = np.around(y_train)
+y_train = y_train.astype(int)
 
-
-model.add(tf.keras.layers.Conv2D(1, kernel_size=kernel_size, name='conv1_1', input_shape = input_shape))
-model.add(tf.keras.layers.Activation("relu"))
-model.add(tf.keras.layers.Conv2D(32, kernel_size=kernel_size, name='conv1_2'))
-model.add(tf.keras.layers.Activation("relu"))
-model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-model.add(tf.keras.layers.Dropout(0.25))
-print("Hola")
+# model.add(tf.keras.layers.Conv2D(32, kernel_size=kernel_size, name='conv1_1', input_shape = input_shape))
+# model.add(tf.keras.layers.LeakyReLU())
+# model.add(tf.keras.layers.Conv2D(64, kernel_size=kernel_size, name='conv1_2'))
+# model.add(tf.keras.layers.LeakyReLU())
+# model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+# model.add(tf.keras.layers.Dropout(0.25))
+# print("Hola")
 # #2
-# model.add(tf.keras.layers.Conv2D(64, kernel_size=kernel_size,name='conv2_1'))
-# model.add(tf.keras.layers.Activation("relu"))
-# model.add(tf.keras.layers.Conv2D(64, kernel_size=kernel_size,name='conv2_2'))
-# model.add(tf.keras.layers.Activation("relu"))
-# model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-# model.add(tf.keras.layers.Dropout(0.25))
-# #3
-# model.add(tf.keras.layers.Conv2D(128, kernel_size=kernel_size,name='conv3_1'))
-# model.add(tf.keras.layers.Activation("relu"))
-# model.add(tf.keras.layers.Conv2D(128, kernel_size=kernel_size,name='conv3_2'))
-# model.add(tf.keras.layers.Activation("relu"))
-# model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-# model.add(tf.keras.layers.Dropout(0.25))
+# # model.add(tf.keras.layers.Conv2D(128, kernel_size=kernel_size,name='conv2_1'))
+# # model.add(tf.keras.layers.Activation("relu"))
+# # # model.add(tf.keras.layers.Conv2D(64, kernel_size=kernel_size,name='conv2_2'))
+# # # model.add(tf.keras.layers.Activation("relu"))
+# # model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+# # model.add(tf.keras.layers.Dropout(0.25))
+# # #3
+# # model.add(tf.keras.layers.Conv2D(128, kernel_size=kernel_size,name='conv3_1'))
+# # model.add(tf.keras.layers.Activation("relu"))
+# # model.add(tf.keras.layers.Conv2D(128, kernel_size=kernel_size,name='conv3_2'))
+# # model.add(tf.keras.layers.Activation("relu"))
+# # model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
+# # model.add(tf.keras.layers.Dropout(0.25))
 
-model.add( tf.keras.layers.Flatten())
-model.add(tf.keras.layers.Dropout(0.5))
+# model.add( tf.keras.layers.Flatten())
+# model.add(tf.keras.layers.Dropout(0.5))
 
-model.add(tf.keras.layers.Dense(numFinalNeurons))
-model.add(tf.keras.layers.Activation('softmax'))
-
-
-# optimizer = tf.train.AdamOptimizer(learning_rate=0.0005)
-model.compile(optimizer='adam',
-              loss='categorical_crossentropy',
-              metrics=['accuracy'])
-
-# #tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
-x_test = cv2.imread("images/661.jpg",0)
-x_test = np.array(x_test).reshape(1, height, width, 1)
-x_test = np.dot(x_test, 1/255)
-
-model.fit(x_train, y_train, epochs=10, shuffle=True, use_multiprocessing=True, verbose=True)
-
-model.evaluate(x_train, y_train)
-pred = model.predict(x_test,verbose=1)
-print(pred)
+# model.add(tf.keras.layers.Dense(numFinalNeurons))
+# model.add(tf.keras.layers.Activation("softmax"))
 
 
-# SAVE MODEL----------------------------
-# serialize model to JSON
-model_json = model.to_json()
-with open("modelNumImages2.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("modelNumImages2.h5")
-print("Saved model to disk")
+# # optimizer = tf.train.AdamOptimizer(learning_rate=0.001)
+# model.compile(optimizer='sgd',
+#               loss='categorical_crossentropy',
+#               metrics=['accuracy'])
+
+# # #tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+# numImage = 0
+# x_test = cv2.imread("images/{}.jpg".format(numImage),0)
+# x_test = np.array(x_test).reshape(1, height, width, 1)
+# x_test = np.dot(x_test, 1/255)
+
+# model.fit(x=x_train, y=y_train, batch_size=32, epochs=20, shuffle=True, use_multiprocessing=True, verbose=True)
+
+# model.evaluate(x_train, y_train)
+# pred = model.predict(x_test,batch_size=32,verbose=1)
+# print(pred)
+
+
+# # SAVE MODEL----------------------------
+# # serialize model to JSON
+# model_json = model.to_json()
+# with open("modelNumImages.json", "w") as json_file:
+#     json_file.write(model_json)
+# # serialize weights to HDF5
+# model.save_weights("modelNumImages.h5")
+# print("Saved model to disk")
 # ------------------------------------------
 
 # LOAD MODEL----------------------------
-# json_file = open('modelNumImages.json', 'r')
-# loaded_model_json = json_file.read()
-# json_file.close()
-# model = tf.keras.models.model_from_json(loaded_model_json)
-# # load weights into new model
-# model.load_weights("modelNumImages.h5")
-# print("Loaded model from disk")
+json_file = open('modelNumImages.json', 'r')
+loaded_model_json = json_file.read()
+json_file.close()
+model = tf.keras.models.model_from_json(loaded_model_json)
+# load weights into new model
+model.load_weights("modelNumImages.h5")
+print("Loaded model from disk")
  
-# # # evaluate loaded model on test data
-# model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-# x_test = cv2.imread("images/76.jpg",0)
-# x_test = np.array(x_test).reshape(1, height, width, 1)
-# x_test = np.dot(x_test, 1/255)
-# pred = model.predict(x_test,verbose=1)
-# print(pred)
+# # evaluate loaded model on test data
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+# model.evaluate(x_train, y_train)
+
+x_test = cv2.imread("images/{}.jpg".format(numImage),0)
+x_test = np.array(x_test).reshape(1, height, width, 1)
+x_test = np.dot(x_test, 1/255)
+x_test = np.around(x_test)
+x_test = x_test.astype(int)
+pred = model.predict(x_test,verbose=1)[0]
+print(pred)
+for i in range(len(pred)):
+  if(pred[i] > 0.01):
+    pred[i] = 1
+  else:
+    pred[i] = 0
+print(pred)
+out = pred.dot(2**np.arange(pred.size)[::-1])
+print(out)
 #------------------------------------
 
 
